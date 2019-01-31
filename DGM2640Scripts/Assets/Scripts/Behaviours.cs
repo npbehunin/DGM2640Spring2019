a53db2b65dfb37ad90eb8ba5cc6ef195dbbada14
npbehunin <npbehunin@gmail.com>
2019-01-31 14:45:50 -0700

    Edited pixar fox design and added new Unity project

---
+++ DGM2640Scripts/Assets/Scripts/Behaviours.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviours : MonoBehaviour
+{
+
+	public UnityEvent OnAwake, OnStart, TriggerEnter, CollisionEnter;
+	
+	private void Awake()
+	{
+		
+	}
+	
+	void Start()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		
+	}
+
+	private void OnDestroy()
+	{
+		
+	}
+}
