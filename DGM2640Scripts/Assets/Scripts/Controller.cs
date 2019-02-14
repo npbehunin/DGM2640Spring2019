c62df4ee349aa95d4444556f39b579793d687625
npbehunin <npbehunin@gmail.com>
2019-02-14 15:39:44 -0700

    Made changes to fox spine and added character controller script tests

---
+++ DGM2640Scripts/Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+	public float MoveSpeed = 5;
+	public float Gravity = -9.81f;
+
+	private CharacterController cc;
+	private Vector3 position;
+	
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		position.x = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+		if (cc.isGrounded)
+		{
+			if (Input.GetKey(KeyCode.Space))
+			{
+				position.y += 100;
+			}
+		}
+		
+		cc.Move(position);
+	}
+}
+
+//Simple character controller script.
+//We don't need rotation in this script because our "Flip" script takes care of it. In another third person controller
+//it will probably need rotation.
---
+++ DGM2640Scripts/Assets/Scripts/Flip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+
+	private Vector3 rot;
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			rot.y = 180;
+			transform.rotation = Quaternion.Euler(rot);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.rotation = Quaternion.identity; //Being at zero
+		}
+	}
+}
+
+//Simple script that flips our character 180 degrees. Useful for sidescrollers.
